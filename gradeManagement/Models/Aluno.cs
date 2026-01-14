fcb194969b8f2b2c9de2a4e1ceddf3b59cac8848
Marney Melo <marney.melo@outlook.com>
2026-01-14 01:29:19 -0300

    feat: implement grade and frequency calculation logic. format output according to problem requirements

--- gradeManagement/Models/Aluno.cs
+++ gradeManagement/Models/Aluno.cs
@@ -9,7 +9,7 @@ public class Aluno
     public double[] Notas { get; set; } = new double[5];
     public double Frequencia { get; set; }
     
-    //robustez para previnir erros de calculo causados pela entrada.
-    // uso do Linq para calcular este valor.
+    // Robustez para previnir erros de calculo causados pela entrada.
+    // Uso do Linq para calcular este valor.
     public double MediaNotas => Notas.Length > 0 ? Notas.Average() : 0;
 }
--- gradeManagement/Program.cs
+++ gradeManagement/Program.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Linq;
-using System.Text;
-using gradeManagement.Models;
+﻿using gradeManagement.Models;
+using gradeManagement.Services;
+using System;
 using System.Globalization; //Para robustez da leitura dos dados
+using System.Text;
 
 List<Aluno> listaAlunos = new List<Aluno>();
+CalculadoraEscolar calculadora = new CalculadoraEscolar();
 
 Console.WriteLine("Insira os dados dos alunos conforme modelo: \n " +
     "<nome_aluno> <nota1> <nota2> <nota3> <nota4> <nota5> <frequencia%> \n" +
@@ -17,15 +18,15 @@ while (true)
     // Condição de parada: se o usuário digitar -1, encerra o loop, sinalizando o fim da leitura.
     if (entrada == "-1") break;
 
-    try 
+    try
     {
         // Quebra strings por espaços
         string[] partes = entrada.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-        
+
         // Cria o objeto Aluno e preenche os dados
         Aluno aluno = new Aluno();
         aluno.Nome = partes[0];
-        
+
         // Converte as 5 notas lidas como string para double
         for (int i = 0; i < 5; i++)
         {
@@ -42,4 +43,23 @@ while (true)
     {
         Console.WriteLine("Erro na leitura dos dados. Tente novamente ou digite -1 para sair.");
     }
+}
+if (listaAlunos.Any()) // if (listaAlunos.Count > 0) tmb funciona
+{
+    // Console.WriteLine("Nome, Média e Frequência por aluno");
+    foreach (var aluno in listaAlunos)
+    {
+        Console.WriteLine($"{aluno.Nome} {aluno.MediaNotas:F0} {aluno.Frequencia}%");
+    }
+    //Console.WriteLine("Média da turma em cada disciplina:");
+    var mediasTurma = calculadora.CalcularMediasPorDisciplina(listaAlunos);
+    Console.WriteLine(string.Join(" ", mediasTurma.Select(m => m.ToString("F0"))));
+
+    //Console.WriteLine("Alunos Acima da Média da turma:");
+    var acimaMedia = calculadora.ObterAlunosAcimaDaMedia(listaAlunos);
+    Console.WriteLine(acimaMedia.Any() ? string.Join(", ", acimaMedia) : "");
+
+    // Console.WriteLine("Alunos com frequência abaixo de 75%:");
+    var baixaFreq = calculadora.ObterAlunosBaixaFrequencia(listaAlunos);
+    Console.WriteLine(baixaFreq.Any() ? string.Join(", ", baixaFreq) : "");
 }
